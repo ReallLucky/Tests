186c69776f92078cf3a43fa3a2165b90a7588d5e
luckyfake <noel@sliwczuk.de>
2026-02-16 13:04:05 +0100

    fix

--- object_spotter.py
+++ object_spotter.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageOps
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("keras_model.h5", compile=False)
 
 # Load labels
 class_names = open("labels.txt", "r").readlines()
